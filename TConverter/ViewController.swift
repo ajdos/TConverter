03ee57b91be4d3f11a559ff9dead6204a76ddf2c
Айдин Абдурахманов <aidinabdurakhmanov@MacBook-Air-Ajdin.local>
2019-01-02 00:49:58 +0300

    First commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Айдин Абдурахманов on 01/01/2019.
+//  Copyright © 2019 Айдин Абдурахманов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+    }
+}
+
